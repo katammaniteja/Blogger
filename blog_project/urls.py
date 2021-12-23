71bf4e6b2d5bb98e4cf01c9cf31ea3ebd1e864a9
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-24 01:04:00 +0530

    Add files via upload

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +13,9 @@ urlpatterns = [
     path('blog/',include('blog_app.urls')),
     path('',views.home,name='index'),
     path('oauth/', include('social_django.urls', namespace='social')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns+=staticfiles_urlpatterns()
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
